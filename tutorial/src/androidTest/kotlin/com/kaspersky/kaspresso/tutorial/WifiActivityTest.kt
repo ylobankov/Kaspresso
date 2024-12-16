0537f1f19bafce791ed1d15c700bd67f4ecffc41
Yaroslav Lobankov <yaroslav.lobankov@wheely.com>
2024-12-16 10:52:46 +0300

    tutorial: refactor WifiActivityTest test
    Use the `before`, `after`, `run` functions and test steps to increase
    test readability.

--- tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/WifiActivityTest.kt
+++ tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/WifiActivityTest.kt
@@ -1,10 +1,12 @@
 package com.kaspersky.kaspresso.tutorial
 
+import android.content.res.Configuration
 import androidx.test.ext.junit.rules.activityScenarioRule
 import com.kaspersky.kaspresso.device.exploit.Exploit
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import com.kaspersky.kaspresso.tutorial.screen.MainScreen
 import com.kaspersky.kaspresso.tutorial.screen.WifiActivityScreen
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,34 +17,52 @@ class WifiActivityTest : TestCase() {
 
     @Test
     fun wifiTest() {
-        MainScreen {
-            wifiActivityButton {
-                isVisible()
-                isClickable()
-                containsText("INTERNET AVAILABILITY")
-                click()
-            }
-        }
-
-        WifiActivityScreen {
-            checkWifiButton {
-                isVisible()
-                isClickable()
-                containsText("CHECK WIFI STATUS")
-            }
-            wifiStatus.hasEmptyText()
-
-            device.network.toggleWiFi(true)
+        before {
             device.exploit.setOrientation(Exploit.DeviceOrientation.Portrait)
-
-            checkWifiButton.click()
-            wifiStatus.hasText(R.string.enabled_status)
-
-            device.network.toggleWiFi(false)
-            device.exploit.rotate()
-
-            checkWifiButton.click()
-            wifiStatus.hasText(R.string.disabled_status)
+            device.network.toggleWiFi(true)
+        }.after {
+            device.exploit.setOrientation(Exploit.DeviceOrientation.Portrait)
+            device.network.toggleWiFi(true)
+        }.run {
+            step("Open target screen") {
+                MainScreen {
+                    wifiActivityButton {
+                        isVisible()
+                        isClickable()
+                        containsText("INTERNET AVAILABILITY")
+                        click()
+                    }
+                }
+            }
+            step("Check WIFI status button") {
+                WifiActivityScreen {
+                    checkWifiButton {
+                        isVisible()
+                        isClickable()
+                        containsText("CHECK WIFI STATUS")
+                    }
+                    wifiStatus.hasEmptyText()
+                }
+            }
+            step("Check WIFI status is enabled") {
+                WifiActivityScreen {
+                    checkWifiButton.click()
+                    wifiStatus.hasText(R.string.enabled_status)
+                }
+            }
+            step("Disable WIFI and rotate device") {
+                device.network.toggleWiFi(false)
+                device.exploit.rotate()
+                Assert.assertTrue(
+                    device.context.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+                )
+            }
+            step("Check WIFI status is disabled") {
+                WifiActivityScreen {
+                    checkWifiButton.click()
+                    wifiStatus.hasText(R.string.disabled_status)
+                }
+            }
         }
     }
 }
